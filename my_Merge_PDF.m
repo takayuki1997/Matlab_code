7231a12ee51de5a59346e503b0f288c713a3362a
Takayuki_SATO <takayuki@brain.riken.jp>
2016-07-26 20:19:46 +0900

    remove file_names variable
    and fix spell merge

--- my_Merge_PDF.m
+++ my_Merge_PDF.m
@@ -1,41 +1,40 @@
-function my_Merge_PDF(merged_file,varargin)
+function my_Merge_PDF(merged_file, varargin)
 % merge multiple PDF files
 % by Takayuki 160726
 % first argument is merged output file name.
 % second and third or after them are input file names.
-% my_Marge_PDF('out.pdf', 'in1.pdf', 'in2.pdf');
+% my_Merge_PDF('out.pdf', 'in1.pdf', 'in2.pdf');
 %
 % input file name is not cell
 % Please convert cell to strings by following command.
 % ins = {'in1.pdf', 'in2.pdf'};
-% my_Marge_PDF('out.pdf', ins{:});
+% my_Merge_PDF('out.pdf', ins{:});
 
 
-rm_option = 1;  % 1:erase originals, 0:leave originals
+rm_option = 0;  % 1:erase originals, 0:leave originals
 
 
-%% 
+%% OS, file, directory check
 
-fprintf('\n----------------\n%s\n',mfilename);
+fprintf('\n----------------\n%s\n', mfilename);
 if ~ismac
-    fprintf('<<This function is only for Mac.>>\n\n');
+    fprintf(' <<This function is only for Mac.>>\n <<PDF files were not merged.>>\n\n');
     return
 end
 
-file_names = cat(2,merged_file,varargin);
 fprintf('\nsource PDF files\n');
-fprintf(' %s\n',file_names{2:end});
+fprintf(' %s\n', varargin{:});
 
 
-%% Marge PDF files
+%% Merge PDF files
 
 merge_command = '/System/Library/Automator/Combine\ PDF\ Pages.action/Contents/Resources/join.py --output';
-merge_command = [merge_command, sprintf(' ''%s''',file_names{:})];
+merge_command = [merge_command, sprintf(' ''%s''', merged_file), sprintf(' ''%s''', varargin{:})];
 
-fprintf('\nmarge command\n %s\n...',merge_command);
+fprintf('\nmerge command\n %s\n...', merge_command);
 unix(merge_command);    % merge here ================
 
-fprintf('\b\b\b\nmarged PDF file\n %s\n\n',file_names{1});
+fprintf('\b\b\b\nmerged PDF file\n %s\n\n', merged_file);
 
 
 %% Delete source PDF files
@@ -43,8 +42,8 @@ fprintf('\b\b\b\nmarged PDF file\n %s\n\n',file_names{1});
 if rm_option
     rm_command = 'rm';
     fprintf('delete source PDF files\n');
-    rm_command = [rm_command, sprintf(' ''%s''',file_names{2:end})];
+    rm_command = [rm_command, sprintf(' ''%s''', varargin{:})];
     unix(rm_command);   % delete here ================
-    fprintf(' %s\n',file_names{2:end});
+    fprintf(' %s\n', varargin{:});
     fprintf('\n');
 end
