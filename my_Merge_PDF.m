f9b0d3df15f7fb832e087ccbf95f6bac6bba7e26
Takayuki_SATO <takayuki@brain.riken.jp>
2016-07-27 18:56:20 +0900

    file exist check

--- my_Merge_PDF.m
+++ my_Merge_PDF.m
@@ -11,22 +11,41 @@ function my_Merge_PDF(merged_file, varargin)
 % my_Merge_PDF('out.pdf', ins{:});
 
 
-rm_option = 0;  % 1:erase originals, 0:leave originals
+rm_option = 1;  % 1:erase originals, 0:leave originals
 
 
 %% OS, file, directory check
 
-fprintf('\n----------------\n%s\n', mfilename);
+fprintf('\n----------------\n%s\n\n', mfilename);
 if ~ismac
     fprintf(' <<This function is only for Mac.>>\n <<PDF files were not merged.>>\n\n');
     return
 end
 
-fprintf('\nsource PDF files\n');
-fprintf(' %s\n', varargin{:});
+flag = 0;
+for m=1:length(varargin)
+    if ~exist(varargin{m}, 'file');
+        fprintf(' Not exist in file ''%s''\n',varargin{m});
+        flag = 1;
+    end
+end
+[pathstr, name, ext] = fileparts(merged_file);
+if isempty(pathstr)
+    pathstr = './';
+end
+if ~isdir(pathstr)
+    fprintf(' Not exist out dir ''%s''\n', pathstr);
+end
+if flag
+    fprintf('\n');
+    return
+end
 
 
 %% Merge PDF files
+
+fprintf('source PDF files\n');
+fprintf(' %s\n', varargin{:});
 
 merge_command = '/System/Library/Automator/Combine\ PDF\ Pages.action/Contents/Resources/join.py --output';
 merge_command = [merge_command, sprintf(' ''%s''', merged_file), sprintf(' ''%s''', varargin{:})];
